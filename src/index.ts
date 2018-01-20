266992c8a1e3f9afc5667bc5fc8956983a183796
Geeku <ggongpei@gmail.com>
2018-01-21 00:02:46 +0800

    Some class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,69 @@
 import zrender from 'zrender';
 
 
-// const zr = zrender.init(document.querySelector('main'), { renderer: 'svg' });
-const zr = zrender.init(document.querySelector('main'));
+const zr = zrender.init(document.querySelector('main'), { renderer: 'svg' });
+// const zr = zrender.init(document.querySelector('main'));
 const w = zr.getWidth();
 const h = zr.getHeight();
-const pool = [];
+const barragePool = [];
+const elPool = [];
 
-for (let i = 0; i < 100; i++) {
+class ContentQueue {
+    private pool = [];
+    getOne () {
+        if (!this.pool.length) return null;
+        return this.pool.splice(this.pool.length - 1, 1);
+    }
+    add (content) {
+        this.pool.push(content);
+    }
+}
+
+class BarragePool {
+    private pool = [];
+    
+    public size: number = 0;
+
+    constructor (size) {
+        this.size = size;
+    }
+
+    init () {
+        for (let i = 0; i < this.size; i++) {
+            const randomY = h * Math.random();
+            const txt = new zrender.Text({
+                style: {
+                    text: 'zrender',
+                    textAlign: 'center',
+                    textVerticalAlign: 'middle',
+                    fontSize: 20,
+                    fontFamily: 'Lato',
+                    fontWeight: 'bolder',
+                    textFill: '#0ff',
+                    blend: 'lighten'
+                },
+                position: [w * 2, randomY]
+            });
+            this.pool.push({
+                el: txt,
+                y: randomY
+            });
+        }
+    }
+
+    *[Symbol.iterator] () {
+        const l = this.pool.length;
+        for (let i = 0; i < l; i++) {
+            yield this.pool[i];
+        }
+    }
+}
+
+class BarrageManager {
+
+}
+
+for (let i = 0; i < 50; i++) {
     const randomY = h * Math.random();
     const txt = new zrender.Text({
         style: {
@@ -23,7 +79,7 @@ for (let i = 0; i < 100; i++) {
         position: [w * 2, randomY]
     });
     zr.add(txt);
-    pool.push({
+    elPool.push({
         el: txt,
         y: randomY
     });
@@ -36,8 +92,8 @@ function loop ({el, y}) {
         position: [-txtRect.width, y]
     }, 3000, 1000 * Math.random());
 }
-pool.forEach(loop);
+elPool.forEach(loop);
 
 setTimeout(() => {
-    pool.forEach(loop);
+    elPool.forEach(loop);
 }, 5000);
